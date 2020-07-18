ba19f00d6fe0c153a3221c8db5d0a261512fed90
Yves Shema <admin@rysapp.com>
2020-07-17 21:54:49 -0700

    choice@0.1.0 published!

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import bool from './bool';
 import float from './float';
 import integer from './integer';
 
-const choice = (max = 1, min = 0, options = {}) => {
+export const choice = (max = 1, min = 0, options = {}) => {
   if (options.float === true) {
     return float(max, min);
   } else if (options.bool === true) {
@@ -11,5 +11,3 @@ const choice = (max = 1, min = 0, options = {}) => {
     return integer(max, min);
   }
 }
-
-export default choice;
